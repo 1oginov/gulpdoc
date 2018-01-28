2b67fd5beda11706c29728bef0f3643efc92f68e
Danila Loginov <danila@loginov.rocks>
2018-01-29 01:20:08 +0300

    feat: implement Gulpdoc class

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # gulpdoc
 
+[![dependencies Status](https://david-dm.org/1oginov/gulpdoc/status.svg)](https://david-dm.org/1oginov/guldoc)
 [![devDependencies Status](https://david-dm.org/1oginov/gulpdoc/dev-status.svg)](https://david-dm.org/1oginov/guldoc?type=dev)
+
+Documentation generator for Gulp tasks.
+
+## Quick Start
+
+Install package:
+
+```sh
+npm install gulpdoc
+```
+
+Use in code:
+
+```js
+const gulpdoc = require('gulpdoc');
+const tasks = gulpdoc.getTasks('./gulp/*.js');
+```
---
+++ src/Gulpdoc.js
@@ -0,0 +1,32 @@
+const CommentsExtractor = require('comments-extractor');
+
+const Task = require('./Task');
+
+/**
+ * Gulpdoc class.
+ */
+class Gulpdoc {
+  /**
+   * Get tasks.
+   * @param {string} pattern Glob pattern
+   * @param {string} [annotation='gulptask'] Annotation
+   * @param {Object} [options={}] Glob options
+   * @return {Array}
+   */
+  static getTasks(pattern, annotation = 'gulptask', options = {}) {
+    const extractor = new CommentsExtractor(pattern, annotation, options);
+    const comments = extractor.extract();
+    const tasks = [];
+
+    comments.forEach((fileComments) => {
+      fileComments.forEach((comment) => {
+        const task = new Task(comment, annotation);
+        tasks.push(task);
+      });
+    });
+
+    return tasks;
+  }
+}
+
+module.exports = Gulpdoc;
